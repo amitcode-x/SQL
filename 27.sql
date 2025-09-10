61bd7b2d5141ace2e1f1a6511572b4dc8d422db9
amitcoderhub <amitchauhan6599@gmail.com>
2025-09-10 22:35:23 +0530

    27

---
+++ 27.sql
@@ -0,0 +1,8 @@
+---WAQTD employees and their department location where salary is greater than 1500.
+
+select ename,
+       loc
+  from emp,
+       dept
+ where emp.deptno = dept.deptno
+   and emp.sal > 1500;
