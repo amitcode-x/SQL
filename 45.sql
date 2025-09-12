684132a020666b3207ad5486bc890a3393d8ffa1
amitcoderhub <amitchauhan6599@gmail.com>
2025-09-12 23:05:03 +0530

    45

---
+++ 45.sql
@@ -0,0 +1,15 @@
+-- 45. Write a SQL query to find the names of employees who earn the second highest salary.
+select ename
+  from emp
+ where sal in (
+   select max(sal)
+     from emp
+    where sal < (
+      select max(sal)
+        from emp
+       where sal < (
+         select max(sal)
+           from emp
+      )
+   )
+);
