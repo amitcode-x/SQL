c3c83b953b5a5aa9e01721e6ad020bc67df45a7f
amitcoderhub <amitchauhan6599@gmail.com>
2025-09-12 23:07:27 +0530

    47

--- 47.sql
+++ 47.sql
@@ -1,5 +1,13 @@
 -- 47. Write a SQL query to find the employees who have the lowest salary in each department where the highest salary is greater than 4000.
- SELECT *
- FROM EMP
- WHERE SAL IN (SELECT MIN(SAL) FROM EMP
- WHERE DEPTNO  IN (SELECT DEPTNO FROM EMP GROUP BY DEPTNO HAVING MAX(SAL) > 4000));
+select *
+  from emp
+ where sal in (
+   select min(sal)
+     from emp
+    where deptno in (
+      select deptno
+        from emp
+       group by deptno
+      having max(sal) > 4000
+   )
+);
