7598c0d7cd371a58483c9e60654e1bc0690200c4
amitcoderhub <amitchauhan6599@gmail.com>
2025-09-14 22:24:18 +0530

    73

--- 73.sql
+++ 73.sql
@@ -1,5 +1,8 @@
 -- Write a query to display the employee names and their manager’s salary if both employee and manager have salary greater than 2300.
- SELECT E1.ENAME,E2.SAL
- FROM EMP
- E1, EMP E2
- WHERE E1.MGR = E2.EMPNO AND E1.SAL >2300 AND E2.SAL>2300;
+select e1.ename,
+       e2.sal
+  from emp e1,
+       emp e2
+ where e1.mgr = e2.empno
+   and e1.sal > 2300
+   and e2.sal > 2300;
