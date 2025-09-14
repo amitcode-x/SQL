b024eaa1602ffc0563443e6bd9bc8d3e91d19ca6
amitcoderhub <amitchauhan6599@gmail.com>
2025-09-14 22:28:10 +0530

    73

---
+++ 74.sql
@@ -0,0 +1,7 @@
+-- Write a query to display the employee names and their manager’s hire date if the employee was hired before 01-DEC-1982. ✅
+select e1.ename,
+       e2.hiredate
+  from emp e1,
+       emp e2
+ where e1.mgr = e2.empno
+   and e1.hiredate < '01-DEC-1982';
