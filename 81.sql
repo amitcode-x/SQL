c306e1c09266851ddc7a7b18ee16e76c115de6fe
amitcoderhub <amitchauhan6599@gmail.com>
2025-09-15 21:38:04 +0530

    1

--- 81.sql
+++ 81.sql
@@ -1,4 +1,4 @@
--- Write a query to display the employee names and their manager’s job for all the employees.
+-- Write a query to display the employee names and their manager’s job for all the employees
 SELECT E1.ENAME, E2.JOB 
 FROM EMP E1, EMP E2
 WHERE E1.MGR = E2.EMPNO;
